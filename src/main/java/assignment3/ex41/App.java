d8d713b48620e867b3a6cd8aa944baa461f06338
Sonxaj <jonasturner12@gmail.com>
2021-06-16 12:49:37 -0400

    Finished ex41

--- src/main/java/assignment3/ex41/App.java
+++ src/main/java/assignment3/ex41/App.java
@@ -1,4 +1,22 @@
 package assignment3.ex41;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.io.File;
+
 public class App {
+
+    public static void main(String[] args) {
+
+        NameSorter nameSorter = new NameSorter();
+
+        File inputFile = new File("src/main/java/assignment3/ex41/exercise41_input.txt");
+        List<String> names = new ArrayList<>();
+
+        nameSorter.readNames(inputFile, names);
+
+        nameSorter.sortNames(names);
+
+        nameSorter.printList(names);
+    }
 }
---
+++ src/main/java/assignment3/ex41/NameSorter.java
@@ -0,0 +1,64 @@
+package assignment3.ex41;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+
+public class NameSorter {
+
+
+    // Read from file and add to list
+    public void readNames(File inputFile, List<String> names){
+
+        // Read names from file
+        try {
+            Scanner reader = new Scanner(inputFile);
+
+            // add all to list
+            while(reader.hasNextLine()){
+                names.add(reader.nextLine());
+            }
+
+            reader.close();
+
+        // catch any exceptions
+        }catch (IOException e){
+            System.out.println("An Error has Occurred.");
+            e.printStackTrace();
+        }
+    }
+
+    // Sort list
+    public void sortNames(List<String> names){
+
+        // Use Collections.sort(names)
+        Collections.sort(names);
+    }
+
+
+    // Print list to file
+    public void printList(List<String> names){
+        try{
+            // create new output file
+            FileWriter outputFile = new FileWriter("src/main/java/assignment3/ex41/exercise41_output.txt");
+
+            outputFile.write("Total of " + names.size() + " names\n");
+            outputFile.write("-----------------\n");
+
+            // write names in list
+            for (String i:names) {
+                outputFile.write(i + "\n");
+            }
+
+            outputFile.close();
+
+        // catch any exceptions
+        }catch (IOException e){
+            System.out.println("An Error has Occurred.");
+            e.printStackTrace();
+        }
+    }
+}
